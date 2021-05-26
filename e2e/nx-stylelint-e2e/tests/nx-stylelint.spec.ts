e80c8565911197b3b96b3be8cab54c9691526e26
Phillip Barta <barta.phillip@gmail.com>
2021-05-26 12:11:06 +0200

    test: added e2e tests for the generators

--- e2e/nx-stylelint-e2e/tests/nx-stylelint.spec.ts
+++ e2e/nx-stylelint-e2e/tests/nx-stylelint.spec.ts
@@ -1,57 +1,212 @@
-it.todo('TODO');
-/*import {
+import {
   checkFilesExist,
   ensureNxProject,
   readJson,
   runNxCommandAsync,
+  updateFile,
+  cleanup,
   uniq,
+  runPackageManagerInstall,
 } from '@nrwl/nx-plugin/testing';
+import { NxJsonConfiguration, WorkspaceJsonConfiguration } from '@nrwl/devkit';
 
 describe('nx-stylelint e2e', () => {
-  it('should create nx-stylelint', async (done) => {
-    const plugin = uniq('nx-stylelint');
-    ensureNxProject('nx-stylelint', 'dist/packages/nx-stylelint');
-    await runNxCommandAsync(
-      `generate nx-stylelint:nx-stylelint ${plugin}`
-    );
-
-    const result = await runNxCommandAsync(`build ${plugin}`);
-    expect(result.stdout).toContain('Executor ran');
-
-    done();
-  });
-
-  describe('--directory', () => {
-    it('should create src in the specified directory', async (done) => {
-      const plugin = uniq('nx-stylelint');
-      ensureNxProject(
-        'nx-stylelint',
-        'dist/packages/nx-stylelint'
-      );
-      await runNxCommandAsync(
-        `generate nx-stylelint:nx-stylelint ${plugin} --directory subdir`
-      );
-      expect(() =>
-        checkFilesExist(`libs/subdir/${plugin}/src/index.ts`)
-      ).not.toThrow();
-      done();
+  describe('nx-stylelint:init', () => {
+    beforeAll(() => {
+      cleanup();
     });
+
+    beforeEach(() => {
+      ensureNxProject('nx-stylelint', 'dist/packages/nx-stylelint');
+    });
+
+    it('should initialize nx-stylelint', async () => {
+      await runNxCommandAsync(`generate nx-stylelint:init`);
+
+      expect(() => checkFilesExist('.stylelintrc.json', 'package.json', 'workspace.json', 'nx.json')).not.toThrow();
+
+      const packageJson = readJson('package.json');
+
+      expect(packageJson.devDependencies['nx-stylelint']);
+      expect(packageJson.devDependencies.stylelint).toBeTruthy();
+      expect(packageJson.devDependencies['stylelint-config-idiomatic-order']).toBeTruthy();
+      expect(packageJson.devDependencies['stylelint-config-prettier']).toBeTruthy();
+      expect(packageJson.devDependencies['stylelint-config-standard']).toBeTruthy();
+
+      const stylelintRc = readJson('.stylelintrc.json');
+      expect(stylelintRc).toMatchObject({
+        extends: ['stylelint-config-standard', 'stylelint-config-idiomatic-order', 'stylelint-config-prettier'],
+        ignoreFiles: ['node_modules/**', 'dist/**'],
+        rules: {
+          'order/properties-alphabetical-order': null,
+        },
+      });
+
+      const nxJson: NxJsonConfiguration = readJson('nx.json');
+      expect(nxJson.tasksRunnerOptions.default).toBeTruthy();
+      expect(nxJson.tasksRunnerOptions.default.options.cacheableOperations).toContain('stylelint');
+      expect(nxJson.implicitDependencies['.stylelintrc.json']).toBe('*');
+    }, 60000);
+
+    it('should not update .stylelintrc.json if present', async () => {
+      updateFile(
+        '.stylelintrc.json',
+        JSON.stringify({ extends: ['stylelint-config-standarf'], ignoreFile: ['node_modules/**'], rules: {} })
+      );
+
+      await runNxCommandAsync(`generate nx-stylelint:init`);
+
+      expect(() => checkFilesExist('.stylelintrc.json', 'package.json', 'workspace.json', 'nx.json')).not.toThrow();
+
+      const packageJson = readJson('package.json');
+
+      expect(packageJson.devDependencies['nx-stylelint']);
+      expect(packageJson.devDependencies.stylelint).toBeTruthy();
+      expect(packageJson.devDependencies['stylelint-config-idiomatic-order']).toBeFalsy();
+      expect(packageJson.devDependencies['stylelint-config-prettier']).toBeFalsy();
+      expect(packageJson.devDependencies['stylelint-config-standard']).toBeFalsy();
+
+      const stylelintRc = readJson('.stylelintrc.json');
+      expect(stylelintRc).toMatchObject({
+        extends: ['stylelint-config-standarf'],
+        ignoreFile: ['node_modules/**'],
+        rules: {},
+      });
+
+      const nxJson: NxJsonConfiguration = readJson('nx.json');
+      expect(nxJson.tasksRunnerOptions.default).toBeTruthy();
+      expect(nxJson.tasksRunnerOptions.default.options.cacheableOperations).toContain('stylelint');
+      expect(nxJson.implicitDependencies['.stylelintrc.json']).toBe('*');
+    }, 60000);
   });
 
-  describe('--tags', () => {
-    it('should add tags to nx.json', async (done) => {
-      const plugin = uniq('nx-stylelint');
-      ensureNxProject(
-        'nx-stylelint',
-        'dist/packages/nx-stylelint'
-      );
-      await runNxCommandAsync(
-        `generate nx-stylelint:nx-stylelint ${plugin} --tags e2etag,e2ePackage`
-      );
-      const nxJson = readJson('nx.json');
-      expect(nxJson.projects[plugin].tags).toEqual(['e2etag', 'e2ePackage']);
-      done();
+  describe('nx-stylelint:configuration', () => {
+    beforeAll(() => {
+      cleanup();
+    });
+
+    beforeEach(() => {
+      ensureNxProject('nx-stylelint', 'dist/packages/nx-stylelint');
+      /*
+      updateFile('package.json', (content) => {
+        const packageJson = JSON.parse(content);
+        packageJson.devDependencies = {
+          ...packageJson.devDependencies,
+          '@nrwl/node': packageJson.devDependencies['@nrwl/workspace'],
+        };
+
+        return JSON.stringify(packageJson);
+      });
+      runPackageManagerInstall();*/
+    });
+
+    it('should add a stylelint configuration to a project', async () => {
+      const projName = uniq('nx-stylelint');
+      await runNxCommandAsync(`generate @nrwl/workspace:library --name ${projName}`);
+      await runNxCommandAsync(`generate nx-stylelint:configuration --project ${projName}`);
+
+      expect(() =>
+        checkFilesExist(
+          '.stylelintrc.json',
+          'package.json',
+          'workspace.json',
+          'nx.json',
+          `libs/${projName}/.stylelintrc.json`
+        )
+      ).not.toThrow();
+
+      const packageJson = readJson('package.json');
+
+      expect(packageJson.devDependencies['nx-stylelint']);
+      expect(packageJson.devDependencies.stylelint).toBeTruthy();
+      expect(packageJson.devDependencies['stylelint-config-idiomatic-order']).toBeTruthy();
+      expect(packageJson.devDependencies['stylelint-config-prettier']).toBeTruthy();
+      expect(packageJson.devDependencies['stylelint-config-standard']).toBeTruthy();
+
+      const stylelintRc = readJson('.stylelintrc.json');
+      expect(stylelintRc).toMatchObject({
+        extends: ['stylelint-config-standard', 'stylelint-config-idiomatic-order', 'stylelint-config-prettier'],
+        ignoreFiles: ['node_modules/**', 'dist/**'],
+        rules: {
+          'order/properties-alphabetical-order': null,
+        },
+      });
+
+      const projectStylelintRc = readJson(`libs/${projName}/.stylelintrc.json`);
+      expect(projectStylelintRc).toMatchObject({
+        extends: ['../../.stylelintrc.json'],
+      });
+
+      const nxJson: NxJsonConfiguration = readJson('nx.json');
+      expect(nxJson.tasksRunnerOptions.default).toBeTruthy();
+      expect(nxJson.tasksRunnerOptions.default.options.cacheableOperations).toContain('stylelint');
+      expect(nxJson.implicitDependencies['.stylelintrc.json']).toBe('*');
+
+      const workspaceJson: WorkspaceJsonConfiguration = readJson('workspace.json');
+
+      expect(workspaceJson.projects[projName].targets.stylelint).toMatchObject({
+        executor: 'nx-stylelint:lint',
+        options: {
+          config: `libs/${projName}/.stylelintrc.json`,
+          lintFilePatterns: [`libs/${projName}/**/*.{css,scss,sass,less}`],
+        },
+      });
+    }, 60000);
+
+    describe('--format', () => {
+      it('should add a stylelint configuration to a project and set the specified formatter', async () => {
+        const projName = uniq('nx-stylelint');
+        await runNxCommandAsync(`generate @nrwl/workspace:library --name ${projName}`);
+        await runNxCommandAsync(`generate nx-stylelint:configuration --project ${projName} --format json`);
+
+        expect(() =>
+          checkFilesExist(
+            '.stylelintrc.json',
+            'package.json',
+            'workspace.json',
+            'nx.json',
+            `libs/${projName}/.stylelintrc.json`
+          )
+        ).not.toThrow();
+
+        const packageJson = readJson('package.json');
+
+        expect(packageJson.devDependencies['nx-stylelint']);
+        expect(packageJson.devDependencies.stylelint).toBeTruthy();
+        expect(packageJson.devDependencies['stylelint-config-idiomatic-order']).toBeTruthy();
+        expect(packageJson.devDependencies['stylelint-config-prettier']).toBeTruthy();
+        expect(packageJson.devDependencies['stylelint-config-standard']).toBeTruthy();
+
+        const stylelintRc = readJson('.stylelintrc.json');
+        expect(stylelintRc).toMatchObject({
+          extends: ['stylelint-config-standard', 'stylelint-config-idiomatic-order', 'stylelint-config-prettier'],
+          ignoreFiles: ['node_modules/**', 'dist/**'],
+          rules: {
+            'order/properties-alphabetical-order': null,
+          },
+        });
+
+        const projectStylelintRc = readJson(`libs/${projName}/.stylelintrc.json`);
+        expect(projectStylelintRc).toMatchObject({
+          extends: ['../../.stylelintrc.json'],
+        });
+
+        const nxJson: NxJsonConfiguration = readJson('nx.json');
+        expect(nxJson.tasksRunnerOptions.default).toBeTruthy();
+        expect(nxJson.tasksRunnerOptions.default.options.cacheableOperations).toContain('stylelint');
+        expect(nxJson.implicitDependencies['.stylelintrc.json']).toBe('*');
+
+        const workspaceJson: WorkspaceJsonConfiguration = readJson('workspace.json');
+
+        expect(workspaceJson.projects[projName].targets.stylelint).toMatchObject({
+          executor: 'nx-stylelint:lint',
+          options: {
+            config: `libs/${projName}/.stylelintrc.json`,
+            lintFilePatterns: [`libs/${projName}/**/*.{css,scss,sass,less}`],
+            format: 'json',
+          },
+        });
+      }, 60000);
     });
   });
 });
-*/
