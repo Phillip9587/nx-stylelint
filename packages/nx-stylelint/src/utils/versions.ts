a34175579eca7bb758ce88c419a34c4f386c2755
Phillip Barta <barta.phillip@gmail.com>
2022-05-28 17:20:34 +0200

    feat: support stylelint-config-standard-scss v4

--- packages/nx-stylelint/src/utils/versions.ts
+++ packages/nx-stylelint/src/utils/versions.ts
@@ -2,7 +2,7 @@
 export const stylelintVersion = '^14.5.3';
 export const stylelintConfigPrettierVersion = '^9.0.3';
 export const stylelintConfigStandardVersion = '^25.0.0';
-export const stylelintConfigStandardScssVersion = '^3.0.0';
+export const stylelintConfigStandardScssVersion = '^4.0.0';
 
 /** Stylelint Visual Studio Code plugin identifier */
 export const stylelintVSCodeExtension = 'stylelint.vscode-stylelint';
