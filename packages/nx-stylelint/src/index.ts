d7e2a1c23ab58f9f6b14448908810afe47d7d3c2
Phillip Barta <barta.phillip@gmail.com>
2023-02-16 15:04:59 +0100

    feat: export executor, generators and schemas

--- packages/nx-stylelint/src/index.ts
+++ packages/nx-stylelint/src/index.ts
@@ -1,3 +1,8 @@
-export * from './generators/configuration/generator';
-export * from './generators/init/generator';
-export * from './generators/scss/generator';
+export { lintExecutor } from './executors/lint/executor';
+export type { LintExecutorSchema } from './executors/lint/schema';
+export { configurationGenerator } from './generators/configuration/generator';
+export type { ConfigurationGeneratorSchema } from './generators/configuration/schema';
+export { initGenerator } from './generators/init/generator';
+export type { InitGeneratorSchema } from './generators/init/schema';
+export { scssGenerator } from './generators/scss/generator';
+export type { ScssGeneratorSchema } from './generators/scss/schema';
