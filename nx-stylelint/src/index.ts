8384f82b9bf595d65f4a7aa629e7d7ac85aac635
Phillip Barta <barta.phillip@gmail.com>
2023-10-23 22:02:53 +0200

    fix: remove scss generator reexport

--- nx-stylelint/src/index.ts
+++ nx-stylelint/src/index.ts
@@ -4,5 +4,3 @@ export { configurationGenerator } from './generators/configuration/generator';
 export type { ConfigurationGeneratorSchema } from './generators/configuration/schema';
 export { initGenerator } from './generators/init/generator';
 export type { InitGeneratorSchema } from './generators/init/schema';
-export { scssGenerator } from './generators/scss/generator';
-export type { ScssGeneratorSchema } from './generators/scss/schema';
