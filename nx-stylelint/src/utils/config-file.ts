ad3c262e261663921d31b65b1d313ef8bce43c31
Phillip Barta <barta.phillip@gmail.com>
2024-02-22 11:48:59 +0100

    feat: add create-nodes plugin

--- nx-stylelint/src/utils/config-file.ts
+++ nx-stylelint/src/utils/config-file.ts
@@ -1,6 +1,11 @@
 import { type Tree, joinPathFragments, offsetFromRoot, readJson, writeJson } from '@nx/devkit';
 import type { Config } from 'stylelint';
 import { PROJECT_STYLELINT_CONFIG_SCSS_OVERRIDE, ROOT_STYLELINT_CONFIG, ROOT_STYLELINT_CONFIG_SCSS } from './config';
+import { workspaceRoot } from '@nx/devkit';
+import { cosmiconfig } from 'cosmiconfig';
+import { existsSync } from 'node:fs';
+import { dirname, isAbsolute, join } from 'node:path';
+import { isRelativePath } from './path';
 
 export const STYLELINT_CONFIG_FILE_PATTERN = '.stylelintrc(.(json|yml|yaml|js))?';
 
@@ -32,4 +37,49 @@ export function isCompatibleRootConfig(tree: Tree): boolean {
   if (!tree.exists(STYLELINT_CONFIG_FILE)) return false;
   const config = readJson<Config>(tree, STYLELINT_CONFIG_FILE);
   return config.ignoreFiles === '**/*' || (Array.isArray(config.ignoreFiles) && config.ignoreFiles.includes('**/*'));
+}
+
+const explorer = cosmiconfig('stylelint', { cache: false, stopDir: workspaceRoot });
+
+export async function readAffectingStylelintConfigFiles(filePath: string): Promise<Set<string>> {
+  if (!existsSync(filePath)) return new Set();
+
+  try {
+    const result = await explorer.load(filePath);
+    if (!result) return new Set();
+
+    const stylelintConfigFiles = new Set<string>([result.filepath]);
+
+    if (result.config?.['extends']) {
+      const extendsItems = new Set<string>();
+      if (typeof result.config?.['extends'] === 'string') {
+        extendsItems.add(result.config['extends']);
+      } else if (Array.isArray(result.config?.['extends'])) {
+        for (const value of result.config['extends'].filter((v) => typeof v === 'string')) {
+          extendsItems.add(value);
+        }
+      }
+
+      if (extendsItems.size > 0) {
+        for (const extendFilePath of extendsItems) {
+          if (isAbsolute(extendFilePath)) {
+            for (const value of await readAffectingStylelintConfigFiles(extendFilePath)) {
+              stylelintConfigFiles.add(value);
+            }
+          } else if (isRelativePath(extendFilePath)) {
+            const path = join(dirname(filePath), extendFilePath);
+            for (const value of await readAffectingStylelintConfigFiles(path)) {
+              stylelintConfigFiles.add(value);
+            }
+          }
+        }
+      }
+    }
+
+    return stylelintConfigFiles;
+  } catch (err) {
+    console.error(err);
+    /* empty */
+    throw err;
+  }
 }
