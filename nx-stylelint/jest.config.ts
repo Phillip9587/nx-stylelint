286232ce99ed8d05df91a26a490cd8fc0645be15
Phillip Barta <barta.phillip@gmail.com>
2024-05-07 16:56:07 +0200

    chore(deps): update Nx to v19

--- nx-stylelint/jest.config.ts
+++ nx-stylelint/jest.config.ts
@@ -16,7 +16,6 @@ const config: Config.InitialOptions = {
   moduleFileExtensions: ['ts', 'js'],
   coverageDirectory: '../coverage/nx-stylelint',
   testEnvironment: 'node',
-  prettierPath: require.resolve('prettier-2'),
 };
 
 export default config;
