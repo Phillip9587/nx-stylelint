b9dcc40da34ab4b5f309e9680fdfbec9281354f6
Phillip9587 <barta.phillip@gmail.com>
2025-01-10 12:52:32 +0100

    chore(deps): update dependency nx to v20.3.1

--- eslint.config.js
+++ eslint.config.js
@@ -18,14 +18,14 @@ module.exports = [
   },
   ...compat.config({ extends: ['plugin:@nx/typescript'] }).map((config) => ({
     ...config,
-    files: ['**/*.ts', '**/*.tsx'],
+    files: ['**/*.ts', '**/*.tsx', '**/*.cts', '**/*.mts'],
     rules: {
       ...config.rules,
     },
   })),
   ...compat.config({ extends: ['plugin:@nx/javascript'] }).map((config) => ({
     ...config,
-    files: ['**/*.js', '**/*.jsx'],
+    files: ['**/*.js', '**/*.jsx', '**/*.cjs', '**/*.mjs'],
     rules: {
       ...config.rules,
     },
